44c1cc42614c28b2cdc874f7536545ca9074801c
Nickeron-dev@users <75437052+Nickeron-dev@users.noreply.github.com>
2020-12-04 22:19:18 +0200

    This code is correct for kata(pins 4 and 6)
    It works!

---
+++ IfLengthIsFourReturnTrue.java
@@ -0,0 +1,17 @@
+public class IfLengthIsFourReturnTrue {
+  public static void main(String[] args) {
+    System.out.println(isFour("a23"));
+  }
+  public static boolean isFour(String number) {
+    try { // start with parsing to int to avoid getting not int values
+      int pin = Integer.parseInt(number);
+      if(number.length() == 4 | number.length() == 6) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch(NumberFormatException exc) {
+      return false;
+    }
+  }
+}
