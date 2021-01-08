1547ea2b4da59e6449eecfb78177a513425149e5
Nickeron-dev <koshkin061969@gmail.com>
2021-01-08 11:48:31 +0200

    Finally solved this kata.

--- CountingDuplicates.java
+++ CountingDuplicates.java
@@ -1,24 +1,23 @@
+package classes;
+
 public class CountingDuplicates {
   public static int duplicateCount(String text) {
-    text = text.toLowerCase();
-    System.out.println(text);
-    char[] array = text.toCharArray();
-    int[] temp = new int[text.length()];
+    text = text.toLowerCase(); // to make same letters in different cases equal
     int answer = 0;
-    for(int i = 0; i < array.length; i++) {
-      for(int j = i + 1; j < array.length; j++) {
-        if(array[i] == array[j]) {
-          System.out.println(array[i] + " equals " + array[j]);
-          temp[i] += 1;
-          array[i] = 0;
+    for(int i = 0; i < text.length(); i++) {
+      if(text.charAt(i) == '!') continue; // to avoid ready elements
+      for(int j = i + 1; j < text.length(); j++) {
+        if(text.charAt(i) == text.charAt(j)) {
+          answer += 1; // found equal elements
+          text = text.replace(text.charAt(i), '!');
+          break; // not to repeat if found already
         }
       }
     }
-    int max = 0;
-    for(int i = 0; i < temp.length; i++) {
-      //if(max < temp[i]) max += temp[i];
-      if(temp[i] != 0) max += 1;
-    }
-    return max;
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Repeated letters in 'Invisibility': " + duplicateCount("Invisibility"));
   }
 }
