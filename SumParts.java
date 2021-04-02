65d9d9264084301c4e1c478eaf6606a68e773fb7
Nickeron-dev <koshkin061969@gmail.com>
2021-04-02 21:12:52 +0300

    I continued solving this kata. I solved it and optimized it as I could but it is still not enought.(

--- SumParts.java
+++ SumParts.java
@@ -1,35 +1,16 @@
-import java.util.Arrays;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
 class SumParts {
-
   public static int[] sumParts(int[] ls) {
-    PrintWriter pw = new PrintWriter(System.out, true);
-    pw.println(Arrays.toString(ls));
-    pw.println(ls.length);
     if(ls.length == 0) {
-      int[] answer = {0};
-      return answer;
-    }
-    int lengthList = ls.length;
-    int[] answer = new int[lengthList + 1];
-    int counter = 1;
-    for (int i = 0; i < answer.length; i++) {
-      int sum = 0;
-      for (int k = 0; k < ls.length; k++) {
-        pw.println(ls[k]);
-        sum += ls[k];
-        pw.println("result: " + sum);
-        pw.flush();
+      return new int[] {0};
+    } 
+    int[] answer = new int[ls.length + 1];
+    int sum = 0;
+    for (int k = 0; k < ls.length; k++) {
+      sum = 0;
+      for (int p = k; p < ls.length; p++) {
+        sum += ls[p];
       }
-      answer[i] = sum;
-      int[] newArray = new int[ls.length - 1];
-      for (int m = 1; m < newArray.length; m++) {
-        newArray[m - 1] = ls[m];
-      }
-      ls = newArray;
-      pw.println(Arrays.toString(newArray));
+      answer[k] = sum;
     }
     return answer;
   }
