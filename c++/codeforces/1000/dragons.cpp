a124bb05f159a9987afd2a304576448e6b838668
Nickeron-dev <koshkin061969@gmail.com>
2022-02-14 10:34:59 +0200

    Solved one problem with 1000 difficulty and figured out how to solve the problem that I haven't solved during my last contest.

--- c++/codeforces/1000/dragons.cpp
+++ c++/codeforces/1000/dragons.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
+using namespace std;
+
+int main()
+{
+	long long s, n, dragon_power, dragon_plus;
+	cin >> s >> n;
+	vector<pair<long long, long long>> v;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> dragon_power >> dragon_plus;
+		v.push_back(make_pair(dragon_power, dragon_plus));
+	}
+	
+	sort(v.begin(), v.end());
+	for (auto e : v)
+	{
+		if (e.first < s)
+		{
+			s += e.second;
+		}
+		else
+		{
+			cout << "NO\n";
+			return 0;
+		}
+	}
+	cout << "YES\n";
+	return 0;
+}
