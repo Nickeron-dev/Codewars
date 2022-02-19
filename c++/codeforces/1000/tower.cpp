d5a026226829370cbe64882d1219950ea551f8e4
Nickeron-dev <koshkin061969@gmail.com>
2022-02-19 17:03:45 +0200

    Started solving one problem, but my code on my architecture works in different way on GNU compiler.

---
+++ c++/codeforces/1000/tower.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+	int max, amount = 0;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		int last = upper_bound(arr.begin(), arr.end(), arr[i]) - arr.begin() - 1;
+		//cout << "last: " << last << '\n';
+		max = last - i + 1 > max ? last - i + 1 : max;
+		amount++;
+		i = last;
+	}
+	cout << max << " " << amount << '\n';
+	return 0;
+}
+
