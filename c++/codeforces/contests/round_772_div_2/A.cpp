043033ce88f2ef3aa8b939232d7f3e39f3d61b4f
Nickeron-dev <koshkin061969@gmail.com>
2022-02-20 18:40:02 +0200

    Solved one problem on a contest.

--- c++/codeforces/contests/round_772_div_2/A.cpp
+++ c++/codeforces/contests/round_772_div_2/A.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
+	int t;
+	cin >> t;
+	for (int v = 0; v < t; v++)
+	{
+		int n;
+		cin >> n;
+		vector<int> vec(n);
+		for (int i = 0; i < n; i++)
+		{
+			int val;
+			cin >> val;
+			vec.push_back(val);
+		}
+		int answer = 0;
+		for (int i = 0; i < vec.size(); i++)
+		{
+			answer = answer | vec[i];
+		}
+		cout << answer << endl;
+	}
+	
 	
 	return 0;
 }
---
+++ c++/codeforces/contests/round_772_div_2/B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	for (int m = 0; m < t; m++)
+	{
+		int n;
+		cin >> n;
+		vector<int> vec;
+		for (int i = 0; i < n; i++)
+		{
+			int val;
+			cin >> val;
+			vec.push_back(val);
+		}
+		
+		if (vec.size() < 3)
+		{
+			cout << 0 << '\n';
+			for (int i = 0; i < vec.size(); i++)
+			{
+				cout << vec[i] << " ";
+			}
+		}
+		else
+		{
+			int counter = 0;
+			for (int i = 1; i < vec.size() - 1; i++)
+			{
+				if (vec[i] > vec[i - 1] && vec[i] > vec[i + 1])
+				{
+					if ( (i + 2 < vec.size() && vec[i + 2] > vec[i + 1]) 
+						&& (vec[i - 1] < vec[i + 2]))
+					{
+						vec[i + 1] = vec[i + 2];
+					}
+					else
+					{
+						vec[i + 1] = vec[i];
+					}
+					counter++;
+				}
+			}
+			cout << counter << '\n';
+			for (int i = 0; i < vec.size(); i++)
+			{
+				cout << vec[i] << " ";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
