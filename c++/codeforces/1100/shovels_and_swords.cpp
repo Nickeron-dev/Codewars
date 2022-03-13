d19ec987525f249b41b11fd81c16946eec0764bf
Nickeron-dev <koshkin061969@gmail.com>
2022-03-14 00:40:39 +0100

    Solved one more problem.

---
+++ c++/codeforces/1100/shovels_and_swords.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+bool is_prime(int number);
+int gcd(int a, int b); // greatest common divisor
+int lcm(int a, int b); // least common multiple
+
+void solution(void)
+{
+	int a, b;
+	cin >> a >> b;
+	cout << min(min(a, b), (a + b) / 3) << '\n';
+}
+
+int main(void)
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		solution();
+	}
+	return 0;
+}
+
+
+
+bool is_prime(int number)
+{
+	if (number <= 1)
+	{
+		return false;
+	}
+
+	for (int i = 2; i * i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
+		return abs(a);
+	}
+	return gcd(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+	return a * b / gcd(a, b);
+}
