36a9783bcff2a994ed808c094f606ec21de89b21
Nickeron-dev <koshkin061969@gmail.com>
2022-04-06 21:44:32 +0200

    Started solving a new problem.

---
+++ c++/codeforces/1100/sequential_nim.cpp
@@ -0,0 +1,68 @@
+//#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+
+bool is_prime(int number);
+int gcd(int a, int b); // greatest common divisor
+int lcm(int a, int b); // least common multiple
+
+void solution(void)
+{
+	int n;
+	cin >> n;
+	vector<int> vec;
+	for (int i = 0; i < n; i++)
+	{
+		int val;
+		cin >> val;
+		vec.push_back(val);
+	}
+	
+}
+
+int main(void)
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		solution();
+	}
+	return 0;
+}
+
+
+
+bool is_prime(int number)
+{
+	if (number <= 1)
+	{
+		return false;
+	}
+
+	for (int i = 2; i * i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
+		return abs(a);
+	}
+	return gcd(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+	return a * b / gcd(a, b);
+}
